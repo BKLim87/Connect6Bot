d53a70206651b20e0391027c7b0fb0e70d1623b3
BKLim87 <limbk87@gmail.com>
2016-06-27 18:40:38 +0900

    fix training script

--- RL/src/script/ColdTrainingScript.py
+++ RL/src/script/ColdTrainingScript.py
@@ -17,12 +17,10 @@ from Learning.OnPolicy.GLIE import GLIE
 
 print('start training')
 
-LLATDF1 = LearningObject(LongistLineStateChanger(), ATDFset(GLIE(0)), 1)
-LLATDF09 = LearningObject(LongistLineStateChanger(), ATDFset(GLIE(0)), 0.9)
-LLATDFR1 = LearningObject(LongistLineStateChanger(), ATDFRset(GLIE(0)), 1)
-LLATDFR09 = LearningObject(LongistLineStateChanger(), ATDFRset(GLIE(0)), 0.9)
+LLATDF09 = LearningObject(LongistLineStateChanger(), ATDFset, 0.9, GLIE(0))
+LLATDFR09 = LearningObject(LongistLineStateChanger(), ATDFRset, 0.9, GLIE(0))
 
-BT = BotTraining(LLATDF09, True, LLATDF09, True, 5000)
+BT = BotTraining(LLATDF09, True, LLATDF09, True, 10000)
 BT.start()
-BT = BotTraining(LLATDFR09, True, LLATDFR09, True, 5000)
+BT = BotTraining(LLATDFR09, True, LLATDFR09, True, 10000)
 BT.start()
