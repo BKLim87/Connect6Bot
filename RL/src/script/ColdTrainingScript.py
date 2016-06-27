3c5b812f98122e79aab0b4030e84eba78c4c0698
BKLim87 <limbk87@gmail.com>
2016-06-27 18:38:34 +0900

    fix training script

--- RL/src/script/ColdTrainingScript.py
+++ RL/src/script/ColdTrainingScript.py
@@ -12,20 +12,17 @@ from ActionTypes.ATDFset import ATDFset
 from ActionTypes.ATDFRset import ATDFRset
 from Training.BotTraining import BotTraining
 from StateChanger.LongistLineStateChanger import LongistLineStateChanger
+from Learning.OnPolicy.GLIE import GLIE
 
 
 print('start training')
 
-LLATDF1 = LearningObject(LongistLineStateChanger(), ATDFset(), 1)
-LLATDF09 = LearningObject(LongistLineStateChanger(), ATDFset(), 0.9)
-LLATDFR1 = LearningObject(LongistLineStateChanger(), ATDFRset(), 1)
-LLATDFR09 = LearningObject(LongistLineStateChanger(), ATDFRset(), 0.9)
+LLATDF1 = LearningObject(LongistLineStateChanger(), ATDFset(GLIE(0)), 1)
+LLATDF09 = LearningObject(LongistLineStateChanger(), ATDFset(GLIE(0)), 0.9)
+LLATDFR1 = LearningObject(LongistLineStateChanger(), ATDFRset(GLIE(0)), 1)
+LLATDFR09 = LearningObject(LongistLineStateChanger(), ATDFRset(GLIE(0)), 0.9)
 
-BT = BotTraining(LLATDF1, True, LLATDF1, True, 500)
+BT = BotTraining(LLATDF09, True, LLATDF09, True, 5000)
 BT.start()
-BT = BotTraining(LLATDF09, True, LLATDF09, True, 500)
-BT.start()
-BT = BotTraining(LLATDFR1, True, LLATDFR1, True, 500)
-BT.start()
-BT = BotTraining(LLATDFR09, True, LLATDFR09, True, 500)
+BT = BotTraining(LLATDFR09, True, LLATDFR09, True, 5000)
 BT.start()
